878530d317d409dd6f02ae69e9b71d14c5aa7c75
Taras Tsugrii <taras.tsugriy@gmail.com>
2020-05-05 03:14:14 +0000

    add rviz config

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -3,7 +3,7 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/w
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml " &
 sleep 5
-xterm -e " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service.rviz " &
+xterm -e " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service_robot.rviz " &
 sleep 15
 xterm -e "rosrun add_markers add_markers " &
 sleep 5
