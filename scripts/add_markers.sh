6f21c68a48f8e377975bc9dc5f9ca1d7cc195a3e
Taras Tsugrii <taras.tsugriy@gmail.com>
2020-05-06 22:34:45 +0000

    move world file to a separate directory

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/taras.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/taras.world " &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml " &
 sleep 5
